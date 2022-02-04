722d7a302d851b8ac094a804ebc1f84f93fc5461
4eug <debraheug@gmail.com>
2022-02-04 07:07:01 +0000

    Styling

--- picture-slider/src/App.js
+++ picture-slider/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from 'react';
+import images from './images';
 
 function App() {
   return (
-    <div className="">
-    <h1>Hello</h1>
+    <div className="App">
+    <motion.div className='carousel'>
+      <motion.div drag="x" className='inner-carousel'>
+       {images.map(image => {
+         return(
+           <motion.div className='item'>
+             <img src={image} alt=""/>
+           </motion.div>
+         )
+       })}
+      </motion.div>
+    </motion.div>
    </div>
   );
 }
+
+
 
 export default App;
